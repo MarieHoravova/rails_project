c92d6b7064e67d4fe7f9b353a26af2f95363c8e8
MarieHoravova <horava01@gmail.com>
2024-03-28 16:59:21 +0100

    eighth_commit

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for the draft blog post" do
+    assert BlogPost.new(published_at: nil).draft?
+  end
+
+  test "draft? returns false for the published blog post" do
+    refute BlogPost.new(published_at: 1.year.ago).draft?
+  end
+
+  test "draft? returns false for the scheduled blog post" do
+    refute BlogPost.new(published_at: 1.year.from_now).draft?
+  end
 end
