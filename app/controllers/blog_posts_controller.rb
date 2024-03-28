bdef58afb30b055e94493e253b6e3069a5b4e82d
MarieHoravova <horava01@gmail.com>
2024-03-28 16:16:38 +0100

    seventh_commit

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -5,7 +5,7 @@ class BlogPostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
 
     def index
-        @blog_posts = BlogPost.published
+        @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     end
       
 
