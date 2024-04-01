6e7ca2622acf908760d1a807c714feb68942767d
MarieHoravova <horava01@gmail.com>
2024-04-01 14:31:33 +0200

    Ninth_commit

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,15 +1,45 @@
 require "test_helper"
+  # assert -> it expectst the true
+  # refute -> it expects the false
+
+  # using fixtures, add the fixture name => blog_posts
+  # using fixtures a bit slower, because we're using the db
 
 class BlogPostTest < ActiveSupport::TestCase
   test "draft? returns true for the draft blog post" do
-    assert BlogPost.new(published_at: nil).draft?
+    assert blog_posts(:draft).draft?
   end
 
   test "draft? returns false for the published blog post" do
-    refute BlogPost.new(published_at: 1.year.ago).draft?
+    refute blog_posts(:published).draft?
   end
 
   test "draft? returns false for the scheduled blog post" do
-    refute BlogPost.new(published_at: 1.year.from_now).draft?
+    refute blog_posts(:scheduled).draft?
   end
-end
+
+  test "published? returns true for the published blog post" do
+    assert blog_posts(:published).published?
+  end
+
+  test "published? returns false for the draft blog post" do
+    refute blog_posts(:draft).published?
+  end
+
+  test "published? returns false for the scheduled blog post" do
+    refute blog_posts(:scheduled).published?
+  end
+
+  test "scheduled? returns true for the scheduled blog post" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false for the draft blog post" do
+    refute blog_posts(:draft).scheduled?
+  end
+
+  test "scheduled? returns false for the published blog post" do
+    refute blog_posts(:published).scheduled?
+  end
+
+end  
